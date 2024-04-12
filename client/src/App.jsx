78830e7d1ff67b76f6184dfd38fccdda7e415353
26simran <sp5006@srmist.edu.in>
2024-04-12 22:47:09 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,12 @@ import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+     <BrowserRouter>
+      <Header/>
   <Routes>
      <Route path="/" element={<Home />} />
      <Route path="/sign-in" element={<SignIn />} />
@@ -14,5 +17,6 @@ export default function App() {
      <Route path="/about" element={<About />} />
      <Route path="/profile" element={<Profile />} />
   </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  );
 }
